30218a337b4fb5c58b1535e0bf2f4fa1c1765d3a
LouisJACQUES <jacques.louis2981@yahoo.fr>
2016-11-03 11:23:07 +0100

    hey new cleanup

---
+++ src/main/java/deqo_ljac/SimpleStack.java
@@ -0,0 +1,7 @@
+package deqo_ljac;
+
+/**
+ * Created by 21506354 on 03/11/2016.
+ */
+public interface SimpleStack {
+}
---
+++ src/test/java/deqo_ljac/SimpleStackTest.java
@@ -0,0 +1,26 @@
+package deqo_ljac;
+
+import junit.framework.TestCase;
+
+public class SimpleStackTest extends TestCase {
+
+    public void testIsEmpty() throws Exception {
+
+    }
+
+    public void testGetSize() throws Exception {
+
+    }
+
+    public void testPush() throws Exception {
+
+    }
+
+    public void testPeek() throws Exception {
+
+    }
+
+    public void testPop() throws Exception {
+
+    }
+}
