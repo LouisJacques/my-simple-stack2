5a6eb883a5e9c3ef30b5d86b8c7c9dd233535a81
LouisJACQUES <jacques.louis2981@yahoo.fr>
2016-11-03 11:16:54 +0100

    hey new

---
+++ src/main/java/deqo_ljac/App.java
@@ -0,0 +1,13 @@
+package deqo_ljac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
